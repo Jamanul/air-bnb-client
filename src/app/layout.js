6c9a1557d659c4c590b6ef8ddf3368b06b3c77cd
Jamanul <jk.skb.25@gmail.com>
2024-10-06 00:08:14 +0600

    all done

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,17 +28,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={manrope.className}
-      >
-        <div className="max-w-[1780px] mx-auto bg-white">
-          <Navbar/>
-        </div>
-        <div className="max-w-[1780px] mx-auto min-h-screen bg-white">
-           {children}
-        </div>
-        <Footer/>
-      </body>
-    </html>
+    <body className={`${manrope.className} bg-white text-black`}>
+      <div className="max-w-[1780px] mx-auto bg-white">
+        <Navbar/>
+      </div>
+      <div className="max-w-[1780px] mx-auto min-h-screen bg-white">
+        {children}
+      </div>
+      <Footer/>
+    </body>
+  </html>
   );
 }
